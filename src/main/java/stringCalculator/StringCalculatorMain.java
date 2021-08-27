567868d738ff791fbcab92141cd78fb0de76b8aa
Aayush <19bce501@nirmauni.ac.in>
2021-08-27 20:58:35 +0530

    Task-1 Check for input containing upto two numbers and add them

--- src/main/java/stringCalculator/StringCalculatorMain.java
+++ src/main/java/stringCalculator/StringCalculatorMain.java
@@ -4,7 +4,27 @@ public class StringCalculatorMain {
 	
 	public static int addNumbers(String numbers) {
 		
-		return -1;        
+		int finalResult = 0;
+		if(numbers.isEmpty() || numbers == null) {
+			finalResult = 0;
+		}
+		else if(numbers.contains(",")){    //Add two numbers which are present in Input.
+			String stringNumberArray[] = numbers.split(",");
+			int firstNumber = stringToInteger(stringNumberArray[0]);
+			int secondNumber = stringToInteger(stringNumberArray[1]);
+			finalResult = firstNumber + secondNumber;
+		}
+		else {
+			finalResult = stringToInteger(numbers); 
+		} 
+		return finalResult;  //Return Final result as an integer value.
+		
+	}
+	
+	public static int stringToInteger(String stringValue) {
+		
+		int result = Integer.parseInt(stringValue);
+		return result;
 		
 	}
 	
--- src/test/java/stringCalculator/StringCalculatorJunitTest.java
+++ src/test/java/stringCalculator/StringCalculatorJunitTest.java
@@ -16,8 +16,27 @@ class StringCalculatorJunitTest {
 	}
 	
 	@Test
-	void initialTest() {
-		
+	void checkForEmptyString() {    //Check when String contains empty String
+		String input=new String("");
+		expectedResult=0;
+		actualResult=StringCalculatorMain.addNumbers(input);
+		assertEquals(expectedResult, actualResult, "Should return 0 for an Empty String");
+	}
+	
+	@Test
+	void checkForOneNumber() {    //Check when string contains only one number in it.
+		String input=new String("5");
+		expectedResult=5;
+		actualResult=StringCalculatorMain.addNumbers(input);
+		assertEquals(expectedResult, actualResult, "Should return same value as input String");
+	}
+
+	@Test
+	void checkForTwoNumbers() {    //Check when string contains two numbers in it, separated by comma(,).
+		String input=new String("5,7");
+		expectedResult=12;
+		actualResult=StringCalculatorMain.addNumbers(input);
+		assertEquals(expectedResult, actualResult, "Should return sum of two numbers which are given as input String");
 	}
 
 }
