126c9d759a9ad5d7fc8e26253b049ab0e49cddf1
Aayush <19bce501@nirmauni.ac.in>
2021-08-27 21:49:02 +0530

    Task-4 Accept single custom delimiter with length one & Add unknown amount of numbers

--- src/main/java/stringCalculator/StringCalculatorMain.java
+++ src/main/java/stringCalculator/StringCalculatorMain.java
@@ -8,7 +8,17 @@ public class StringCalculatorMain {
 		if(numbers.isEmpty() || numbers == null) {
 			finalResult = 0;
 		}
-		else if(numbers.contains(",")){    //Add unknown amount of numbers which are present in Input.
+		else if(numbers.startsWith("//")) {    //Separate by single custom delimiter of length one.
+			String delimiter = new String("");
+			delimiter = numbers.substring(2, numbers.indexOf('\n'));
+			if(delimiter.equals(".") || delimiter.equals("*")) {
+				delimiter = "\\" + delimiter;
+			}
+			numbers = numbers.substring(numbers.indexOf('\n') + 1);
+			String stringNumberArray[] = numbers.split(delimiter);
+			finalResult = calculateSum(stringNumberArray);
+		}
+		else if(numbers.contains(",")){ 
 			String stringNumberArray[] = numbers.split("[,\n]");
 			int arrayLength = stringNumberArray.length;
 			int integerNumber = 0;
@@ -22,6 +32,16 @@ public class StringCalculatorMain {
 		} 
 		return finalResult;  //Return Final result as an integer value.
 		
+	}
+	
+	public static int calculateSum(String[] array) {
+		int sum = 0;
+		int integerNumber=0, arrayLength = array.length;
+		for(int index = 0; index < arrayLength; index++) {
+			integerNumber = stringToInteger(array[index]);
+			sum = sum + integerNumber;
+		}
+		return sum;
 	}
 	
 	public static int stringToInteger(String stringValue) {
--- src/test/java/stringCalculator/StringCalculatorJunitTest.java
+++ src/test/java/stringCalculator/StringCalculatorJunitTest.java
@@ -5,29 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+
 class StringCalculatorJunitTest {
 
 	private int expectedResult, actualResult;
 
 	@Before   
 	public void initializeData() {   //This function will run before all the TestCases.
-		expectedResult=0;
-		actualResult=0;
+		expectedResult = 0;
+		actualResult = 0;
 	}
 	
 	@Test
 	void checkForEmptyString() {    //Check when String contains empty String
-		String input=new String("");
-		expectedResult=0;
-		actualResult=StringCalculatorMain.addNumbers(input);
+		String input = new String("");
+		expectedResult = 0;
+		actualResult = StringCalculatorMain.addNumbers(input);
 		assertEquals(expectedResult, actualResult, "Should return 0 for an Empty String");
 	}
 	
 	@Test
 	void checkForOneNumber() {    //Check when string contains only one number in it.
-		String input=new String("5");
-		expectedResult=5;
-		actualResult=StringCalculatorMain.addNumbers(input);
+		String input = new String("5");
+		expectedResult = 5;
+		actualResult = StringCalculatorMain.addNumbers(input);
 		assertEquals(expectedResult, actualResult, "Should return same value as input String");
 	}
 
@@ -41,18 +42,34 @@ class StringCalculatorJunitTest {
 	
 	@Test
 	void checkForMultipleNumbers() {    //Check when string contains unknown amount of numbers, separated by comma(,).
-		String input=new String("5,7,5,3,4,1");
-		expectedResult=25;
-		actualResult=StringCalculatorMain.addNumbers(input);
+		String input = new String("5,7,5,3,4,1");
+		expectedResult = 25;
+		actualResult = StringCalculatorMain.addNumbers(input);
 		assertEquals(expectedResult, actualResult, "Should return sum of all numbers present in input String");
 	}
 	
 	@Test
 	void shouldAcceptNewLineDelimiter() {    //Check when string contains numbers separated by comma(,) and/or new line(\n).
-		String input=new String("5\n7\n5,3,4\n1,10");
-		expectedResult=35;
-		actualResult=StringCalculatorMain.addNumbers(input);
+		String input = new String("5\n7\n5,3,4\n1,10");
+		expectedResult = 35;
+		actualResult = StringCalculatorMain.addNumbers(input);
 		assertEquals(expectedResult, actualResult, "Should return sum of all numbers present in input String and accpet new line delimiter");
+	}
+	
+	@Test
+	void shouldAcceptSingleCustomDelimiter() {    //Check when string contains numbers separated by a single custom delimiter of length one.
+		String input = new String("//;\n1;4");
+		expectedResult = 5;
+		actualResult = StringCalculatorMain.addNumbers(input);
+		assertEquals(expectedResult, actualResult, "Should return sum of all numbers present in input String separated by a single custom delimiter");
+	}
+	
+	@Test
+	void shouldAcceptDotAsCustomDelimiter() {    //Check when string contains numbers separated by dot(.) as a single custom delimiter.
+		String input = new String("//.\n1.4.5");
+		expectedResult = 10;
+		actualResult = StringCalculatorMain.addNumbers(input);
+		assertEquals(expectedResult, actualResult, "Should return sum of all numbers present in input String separated by Dot as a single custom delimiter");
 	}
 
 }
